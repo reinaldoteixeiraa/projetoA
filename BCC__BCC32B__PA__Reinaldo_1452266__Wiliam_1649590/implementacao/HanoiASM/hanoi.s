0283a865cc044447a8d6f0dd4ef155d50bc13541
reinaldoteixeiraa <reinaldoteixeira@alunos.utfpr.edu.br>
2017-10-26 14:36:53 -0200

    continuando hanoiasm

--- BCC__BCC32B__PA__Reinaldo_1452266__Wiliam_1649590/implementacao/HanoiASM/hanoi.s
+++ BCC__BCC32B__PA__Reinaldo_1452266__Wiliam_1649590/implementacao/HanoiASM/hanoi.s
@@ -5,9 +5,35 @@
 	msg_2: .asciiz "Move Disco 1 da torre %c para a torre %c\n"
 	msg_3: .asciiz "Move Disco %d da torre %c para a torre %c\n" 
 	
+	a: .word 0x00000000
+	b: .word 0x00000000
+	c: .word 0x00000000
+	
+	#int discos
+	discos: .word 0
+	
 .text
+ 
+ j main 
+
+hanoi: 
+	
+
+
 
 main:
 	
 	#printf("informe a quantidade de discos: ");
+	la	$a0,msg_1		#carrega o endereco da msg para syscall 
+	li	$v0,4			# print_string é system call 4
+	syscall
+	
+	#scanf("%d", &discos);
+	li $v0, 5			# read_int é system call 5
+	syscall				
+	sw $v0, discos 			#salva o inteiro em v0
+	
+	#chama a funcao hanoi
+	jal hanoi 
+	
 	
